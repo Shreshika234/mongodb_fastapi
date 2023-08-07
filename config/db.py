f7b74be8dbd02f10a19f7cd3f1866a7b87cc7826
Shreshika234 <121421777+Shreshika234@users.noreply.github.com>
2023-08-07 14:43:04 +0530

    added comments

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 con = MongoClient('mongodb+srv://Shreshika:Bunny234@cluster0.lzku1n5.mongodb.net/')
-db=con["student"]
-collection=db["studentdata"]
+db=con["student"]  #db name sutdent
+collection=db["studentdata"]   #collection name studentdata
